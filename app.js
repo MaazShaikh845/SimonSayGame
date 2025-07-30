2b58ff84db1ad041104763e57ddfff46312553c1
ShaikhMaaz <maaz.221842.ci@mhssce.ac.in>
2025-07-31 01:50:08 +0530

    updated from local

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Event bubbling
+// Event bubbling
 // let div = document.querySelector('div');
 // let ul = document.querySelector('ul');
 // let lis = document.querySelectorAll('li');
@@ -48,106 +48,106 @@
     
 // })
 
-//Simon say game
+// Simon say game
 
-// let gameSeq = [];
-// let userSeq = [];
-// let btn = ['red','yellow','green','blue'];
+let gameSeq = [];
+let userSeq = [];
+let btn = ['red','yellow','green','blue'];
 
-// let start = false;
-// let level = 0;
+let start = false;
+let level = 0;
 
-// let h2 = document.querySelector('h2');
-
-
-// document.addEventListener('keypress', function() {
-//     if (start == false) {
-//         console.log("Game started");
-//         start = true;
-
-//         levelUp();
-//     }
-// })
-
-// function btnFlash(btn) {
-//     btn.classList.add('flash');
-//     setTimeout( function() {
-//         btn.classList.remove('flash')
-//     }, 150);
-// }
-
-// function userFlash(btn) {
-//     btn.classList.add('user');
-//     setTimeout( function() {
-//         btn.classList.remove('user')
-//     }, 150);
-// }
-
-// function levelUp() {
-//     userSeq = [];
-//     level++;
-//     h2.innerText = `level ${level}`;
+let h2 = document.querySelector('h2');
 
 
-//     let randomInd = Math.floor( Math.random()*3);
-//     let randColor = btn[randomInd];
-//     let randBtn = document.querySelector(`.${randColor}`);
+document.addEventListener('keypress', function() {
+    if (start == false) {
+        console.log("Game started");
+        start = true;
 
-//     gameSeq.push(randColor);
-//     console.log(gameSeq);
-//     btnFlash(randBtn);
+        levelUp();
+    }
+})
+
+function btnFlash(btn) {
+    btn.classList.add('flash');
+    setTimeout( function() {
+        btn.classList.remove('flash')
+    }, 150);
+}
+
+function userFlash(btn) {
+    btn.classList.add('user');
+    setTimeout( function() {
+        btn.classList.remove('user')
+    }, 150);
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `level ${level}`;
+
+
+    let randomInd = Math.floor( Math.random()*3);
+    let randColor = btn[randomInd];
+    let randBtn = document.querySelector(`.${randColor}`);
+
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    btnFlash(randBtn);
 
 
 
-// }
+}
 
-// function checkAns(indx) {
-//     if (gameSeq[indx] == userSeq[indx]) {
-//         if(gameSeq.length == userSeq.length){
-//             setTimeout(levelUp, 1000);
-//         }
-//     } else {
-//         h2.innerHTML = `Game over!, Your score was <b>${level}</b> <br> press any key to start`;
-//         document.querySelector('body').style.backgroundColor = 'red';
-//         setTimeout( function() {
-//             document.querySelector('body').style.backgroundColor = 'white';
-//         }, 150);
+function checkAns(indx) {
+    if (gameSeq[indx] == userSeq[indx]) {
+        if(gameSeq.length == userSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game over!, Your score was <b>${level}</b> <br> press any key to start`;
+        document.querySelector('body').style.backgroundColor = 'red';
+        setTimeout( function() {
+            document.querySelector('body').style.backgroundColor = 'white';
+        }, 150);
 
-//         heighestScore();
-//         reset();
-//     }
-// }
+        heighestScore();
+        reset();
+    }
+}
 
-// function btnPress() {
-//     let btn = this;
-//     userFlash(btn);
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
 
-//     userColor = btn.getAttribute('id');
-//     userSeq.push(userColor);
+    userColor = btn.getAttribute('id');
+    userSeq.push(userColor);
 
-//     checkAns(userSeq.length-1);
-// }
+    checkAns(userSeq.length-1);
+}
 
-// let btns = document.querySelectorAll('.btn');
+let btns = document.querySelectorAll('.btn');
 
-// for (allBtn of btns) {
-//     allBtn.addEventListener('click', btnPress);
-// }
+for (allBtn of btns) {
+    allBtn.addEventListener('click', btnPress);
+}
 
-// function reset() {
-//     start = false;
-//     gameSeq = [];
-//     userSeq = [];
-//     level = 0;
-// }
+function reset() {
+    start = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
 
-// let max = 0;
-// function heighestScore() {
-//     let score = document.querySelector('.high');
-//     if (level >= max) {
-//         score.innerText = `Heighest score was: ${level}`;
-//         max = level;
-//     } else {
-//         score.innerText = `Heighest score was: ${max}`;
-//     }
-// }
+let max = 0;
+function heighestScore() {
+    let score = document.querySelector('.high');
+    if (level >= max) {
+        score.innerText = `Heighest score was: ${level}`;
+        max = level;
+    } else {
+        score.innerText = `Heighest score was: ${max}`;
+    }
+}
